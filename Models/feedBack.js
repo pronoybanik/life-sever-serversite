7795f370945752e0d899875659a4e6de34072e99
PRONOY <pronoybanik82@gmail.com>
2023-09-20 11:52:52 +0600

    error solve

--- Models/feedBack.js
+++ Models/feedBack.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-
 const feedBackSchema = mongoose.Schema(
     {
         email: {
--- Routes/feedBack.Route.js
+++ Routes/feedBack.Route.js
@@ -3,7 +3,7 @@ const FeedBackController = require("../controllers/feedBack.Controller");
 const router = express.Router();
 
 
-router.route("")
+router.route("/")
     .get(FeedBackController.getFeedBack)
     .post(FeedBackController.createFeedBack);
 
